07ba619d0affdcab4b92f6b798651d1670cc8c16
Da0hn <vegancraft13@gmail.com>
2020-08-19 18:24:28 -0400

    Dependency Inversion Principle, exemplo refatorado

--- src/main/java/org/gabriel/solid/dependency_inversion/Main.java
+++ src/main/java/org/gabriel/solid/dependency_inversion/Main.java
@@ -1,20 +1,21 @@
 package org.gabriel.solid.dependency_inversion;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 /**
  * @author daohn on 19/08/2020
  * @project design-pattern-course
  */
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         var message = new Message("This is a message");
         var printer = new MessagePrinter();
-        printer.writeMessage(message, "src/main/resources/test_msg.json");
-        printer.writeMessage(new Message("316yrgs"), "src/main/resources/test_msg.json");
-        printer.writeMessage(new Message("1564hwrsg36"), "src/main/resources/test_msg.json");
-        printer.writeMessage(new Message("153467"), "src/main/resources/test_msg.json");
-        printer.writeMessage(new Message("rw26yu2524"), "src/main/resources/test_msg.json");
-        printer.writeMessage(new Message("6843uj3h2451"), "src/main/resources/test_msg.json");
+        try(var writer = new PrintWriter("src/main/resources/test_msg.json")) {
+            printer.writeMessage(message, new JSONFormatter(), writer);
+        }
+        catch(IOException | FormatException e) {
+            e.printStackTrace();
+        }
     }
 }
